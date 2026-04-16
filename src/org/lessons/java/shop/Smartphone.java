4dd6b47555bd91a9c9fb5424b940fdd9de429de9
Pat <dmtpatricica@email.com>
2026-04-16 16:00:21 +0200

    smartphone class +construct +getter + setter

---
+++ src/org/lessons/java/shop/Smartphone.java
@@ -0,0 +1,32 @@
+package org.lessons.java.shop;
+
+import java.math.BigDecimal;
+
+public class Smartphone extends Prodotto {
+    private String IMEI;
+    private float memoria;
+
+    public Smartphone(String nome, String marca, BigDecimal prezzo, BigDecimal iva, String IMEI, float memoria) {
+        super(nome, marca, prezzo, iva);
+        this.IMEI = IMEI;
+        this.memoria = memoria;
+    }
+
+    public String getIMEI() {
+        return this.IMEI;
+    }
+
+    private void setIMEI(String IMEI) {
+        this.IMEI = IMEI;
+    }
+
+    public float getMemoiria() {
+        return this.memoria;
+    }
+
+    public void setMemoria(float memoria) {
+        if (memoria >= 0) {
+            this.memoria = memoria;
+        }
+    }
+}
