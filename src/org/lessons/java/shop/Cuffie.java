73a39a54392ba9625be85f43bfde12a9b1826a5b
Pat <dmtpatricica@email.com>
2026-04-16 16:46:25 +0200

    cuffie class + constructor + getter + setter

---
+++ src/org/lessons/java/shop/Cuffie.java
@@ -0,0 +1,35 @@
+package org.lessons.java.shop;
+
+import java.math.BigDecimal;
+
+public class Cuffie extends Prodotto {
+    private String colore;
+    private boolean iswireless;
+
+    //* costruttore------------------------------------------------------------->^.^<-------------------------------------
+    public Cuffie(String nome, String marca, BigDecimal prezzo, BigDecimal iva, String colore, boolean iswireless) {
+        super(nome, marca, prezzo, iva);
+        this.colore = colore;
+        this.iswireless = iswireless;
+    }
+
+    //* getter&setter------------------------------------------------------------->^.^<-------------------------------------
+    public String getColore() {
+        return this.colore;
+    }
+    
+    private void setColore(String colore) {
+        if (colore != null) {
+            this.colore = colore;
+        }
+    }
+
+    public boolean getIsWireless() {
+        return this.iswireless;
+     }
+
+      private void setIsWireless(boolean iswireless) {
+          this.iswireless = iswireless;
+    }
+
+}
