9d8b777ac450f15914c6048db36b8749b19fd18b
Pat <dmtpatricica@email.com>
2026-04-16 16:10:40 +0200

    Telelvisore class + construct + getter + setter

---
+++ src/org/lessons/java/shop/Televisori.java
@@ -0,0 +1,36 @@
+package org.lessons.java.shop;
+
+import java.math.BigDecimal;
+
+public class Televisori extends Prodotto {
+
+    private String dimensioni;
+    private boolean isSmart;
+
+    //* costruttore
+    public Televisori(String nome, String marca, BigDecimal prezzo, BigDecimal iva, String dimensioni,
+            boolean isSmart) {
+        super(nome, marca, prezzo, iva);
+        this.dimensioni = dimensioni;
+        this.isSmart = isSmart;
+    }
+    
+    //* getter setter ---------------------------------->^.^<--------------------------------------------------------
+    public String getDimensioni() {
+        return this.dimensioni;
+    }
+
+    private void setDimensioni(String dimensioni) {
+        if (dimensioni != null) {
+            this.dimensioni = dimensioni;
+        }
+    }
+
+    public boolean getIsSmart() {
+        return this.isSmart;
+    }
+
+    public void setIsSmart(boolean isSmart) {
+        this.isSmart = isSmart;
+    }
+}
